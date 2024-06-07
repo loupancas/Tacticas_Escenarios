8aae9d5454d15a04261572e4cc1e18009ccf5a17
loupancas <129621626+loupancas@users.noreply.github.com>
2024-06-07 16:40:20 -0500

    RenderFeaturesIntercambiables

--- Assets/Shaders/Postprocess/PostVolume.cs
+++ Assets/Shaders/Postprocess/PostVolume.cs
@@ -20,20 +20,7 @@ public class PostVolume : MonoBehaviour
         if (!volume) throw new System.NullReferenceException(nameof(volume));
         if (profile1 == null || profile2 == null || profile3 == null) throw new System.NullReferenceException("Profiles are null");
         SetProfile(currentProfile);
-
-
-        //Visto en Unity Forum
-        //// This is the correct way to get the VolumeProfile component.
-        //// You can also use GetComponentInChildren if the VolumeProfile is a child of the GameObject.
-        //UnityEngine.Rendering.VolumeProfile volumeProfile = GetComponent<UnityEngine.Rendering.Volume>()?.profile;
-        //if (!volumeProfile) throw new System.NullReferenceException(nameof(UnityEngine.Rendering.VolumeProfile));
-        //// You can leave this variable out of your function, so you can reuse it throughout your class.
-        //UnityEngine.Rendering.Universal.Vignette vignette;
-
-        //if (!volumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
-
-        //vignette.intensity.Override(0.5f);
-
+     
     }
 
     private void Update()
