f23d4dd7decfd1ae08c95daff93600f532eee7d8
Arfilion <105883987+Arfilion@users.noreply.github.com>
2024-06-28 14:42:09 -0300

    UI

--- Assets/Scripts/Clases abstractas/Entity.cs
+++ Assets/Scripts/Clases abstractas/Entity.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public abstract class Entity : MonoBehaviour
 {
     [Header("Values of entity")]
     [SerializeField] protected int _vidaMax;
     [SerializeField] protected int _vida;
+    [SerializeField] public VisualEffect _damageParticle;
     public virtual void TakeDamage(int Damage)
     {
         _vida -= Damage;
+        _damageParticle.Play();
+
         if (_vida < 0)
         {
             Morir();
