7c6c760e3a3abc3706f39c4e3eeca412366059d5
nanimore01 <nanimore01@gmail.com>
2024-07-05 05:44:13 -0300

    IA ESTANCADA

--- Assets/Scripts/FSM/Estados del Torturado/AttackTorturado.cs
+++ Assets/Scripts/FSM/Estados del Torturado/AttackTorturado.cs
@@ -7,25 +7,38 @@ public class AttackTorturado : MonoBaseState
 {
     NavMeshAgent _agent;
     Torturado _me;
-    Transform _position;
-    CountdownTimer _timer;
-    bool _OnCharge;
-    [SerializeField] float _speedCharge;
-    [SerializeField] float _timeToCharge;
+    [SerializeField]GameObject _target;
+    CountdownTimer _timerToCharge, _timerOnCharge;
+    [SerializeField] float _maxVelocity;
+    [SerializeField] float _maxForce;
+    Vector3 _velocity;
+    bool _OnCharge = false;
+    [SerializeField] float _distance;
+    [SerializeField] float _timeToCharge, _timeOnCharge;
+    Rigidbody _rb;
 
     private void Start()
     {
-        _agent = gameObject.GetComponent<NavMeshAgent>();
         _me = gameObject.GetComponent<Torturado>();
-        _agent.speed = _speedCharge;
-        
-        _timer = new CountdownTimer(_timeToCharge);
-
-        _timer.OnTimerStart = OnCharge;
-        _timer.OnTimerStop = SetTarget;
-        _timer.Start();
+        _rb = gameObject.GetComponent<Rigidbody>();
     }
 
+    public override void Enter(IState from, Dictionary<string, object> transitionParameters = null)
+    {
+        base.Enter(from, transitionParameters);
+
+        _timerOnCharge = new CountdownTimer(_timeOnCharge);
+
+
+
+        _timerToCharge = new CountdownTimer(_timeToCharge);
+
+        _timerToCharge.OnTimerStart = OnCharge;
+        _timerToCharge.OnTimerStop = SetTarget;
+        _timerToCharge.Start();
+
+        
+    }
     public override IState ProcessInput()
     {
         return this;
@@ -33,31 +46,73 @@ public class AttackTorturado : MonoBaseState
 
     public override void UpdateLoop()
     {
-        _timer.Tick(Time.deltaTime);
-        print(_timer.Progress);
-        if(_OnCharge)
-        {
-            _agent.destination = transform.forward;
-        }
+        _timerToCharge.Tick(Time.deltaTime);
         
-    }
+        if (_OnCharge)
+        {
+            AddForce(_target.transform.position);
 
+            if(Vector3.Distance(transform.position, _target.transform.position) <= 0.5f || _timerOnCharge.IsFinished)
+            {
+                _rb.velocity = Vector3.zero;
+                _OnCharge = false;
+                _timerToCharge.Reset();
+                _timerToCharge.Start();
+            }
+        }
+
+        _me.transform.position += _velocity * Time.deltaTime;
+        _me.transform.forward = _velocity;
+    }
+    
+    
 
     public void OnCharge()
     {
-        transform.LookAt(GameManager.instance.pj.transform.position);
+        transform.LookAt(GameManager.instance.pj.transform);
+        _target.transform.position = new Vector3(GameManager.instance.pj.transform.position.x * _distance, 1, GameManager.instance.pj.transform.position.z * _distance);
+        
     }
 
     public void SetTarget()
     {
+        print("Arranco");
         _OnCharge = true;
-    }
-
-    public override Dictionary<string, object> Exit(IState to)
-    {
-        return base.Exit(to);
-
         
+        //_timerOnCharge.Start();
     }
 
+    Vector3 Seek(Vector3 dir)
+    {
+        var desired = dir - _me.transform.position;
+        desired.Normalize();
+        desired *= _maxVelocity;
+
+        var steering = desired - _velocity;
+        steering = Vector3.ClampMagnitude(steering, _maxForce);
+
+        return steering;
+    }
+    void AddForce(Vector3 dir)
+    {
+        _velocity += dir;
+
+        _velocity = Vector3.ClampMagnitude(_velocity, _maxVelocity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, _target.transform.position);
+
+        //Gizmos.color = isPath ? Color.green : Color.red;
+
+        //Gizmos.DrawSphere(transform.position, 0.5f);
+
+        //Gizmos.color = Color.blue;
+
+        //foreach (var node in neighbour)
+        //{
+        //    Gizmos.DrawLine(transform.position, node.transform.position);
+        //}
+    }
 }
